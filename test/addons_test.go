9c2f141aa8cbd029b17fb3a6de34b5cec38b3e70
Shane Utt <shaneutt@users.noreply.github.com>
2020-02-18 16:52:13 -0500

    [chore] add integration tests and CI (#1)

---
+++ test/addons_test.go
@@ -0,0 +1,203 @@
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/blang/semver"
+	"gopkg.in/yaml.v2"
+	"sigs.k8s.io/kind/pkg/apis/config/v1alpha3"
+	"sigs.k8s.io/kind/pkg/cluster"
+
+	"github.com/mesosphere/kubeaddons/hack/temp"
+	"github.com/mesosphere/kubeaddons/pkg/api/v1beta1"
+	"github.com/mesosphere/kubeaddons/pkg/catalog"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/git"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/local"
+	"github.com/mesosphere/kubeaddons/pkg/test"
+	"github.com/mesosphere/kubeaddons/pkg/test/cluster/kind"
+)
+
+const (
+	defaultKubernetesVersion = "1.16.4"
+	patchStorageClass        = `{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}`
+)
+
+var addonTestingGroups = make(map[string][]string)
+
+func init() {
+	b, err := ioutil.ReadFile("groups.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := yaml.Unmarshal(b, addonTestingGroups); err != nil {
+		panic(err)
+	}
+}
+
+func TestValidateUnhandledAddons(t *testing.T) {
+	unhandled, err := findUnhandled()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(unhandled) != 0 {
+		names := make([]string, len(unhandled))
+		for _, addon := range unhandled {
+			names = append(names, addon.GetName())
+		}
+		t.Fatal(fmt.Errorf("the following addons are not handled as part of a testing group: %+v", names))
+	}
+}
+
+func TestKommanderGroup(t *testing.T) {
+	if err := testgroup(t, "kommander"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// -----------------------------------------------------------------------------
+// Private Functions
+// -----------------------------------------------------------------------------
+
+func testgroup(t *testing.T, groupname string) error {
+	t.Logf("testing group %s", groupname)
+
+	version, err := semver.Parse(defaultKubernetesVersion)
+	if err != nil {
+		return err
+	}
+
+	cluster, err := kind.NewCluster(version, cluster.CreateWithV1Alpha3Config(&v1alpha3.Cluster{}))
+	if err != nil {
+		// try to clean up in case cluster was created and reference available
+		if cluster != nil {
+			_ = cluster.Cleanup()
+		}
+		return err
+	}
+	defer cluster.Cleanup()
+
+	if err := temp.DeployController(cluster, "kind"); err != nil {
+		return err
+	}
+
+	addons, err := addons(addonTestingGroups[groupname]...)
+	if err != nil {
+		return err
+	}
+
+	ph, err := test.NewBasicTestHarness(t, cluster, addons...)
+	if err != nil {
+		return err
+	}
+	defer ph.Cleanup()
+
+	ph.Validate()
+	ph.Deploy()
+
+	return nil
+}
+
+func addons(names ...string) ([]v1beta1.AddonInterface, error) {
+	var testAddons []v1beta1.AddonInterface
+
+	repo, err := local.NewRepository("base", "../addons")
+	if err != nil {
+		return testAddons, err
+	}
+
+	externalRepo, err := git.NewRemoteRepository("https://github.com/mesosphere/kubernetes-base-addons", "master", "origin")
+	if err != nil {
+		return testAddons, err
+	}
+
+	cat, err := catalog.NewCatalog(repo, externalRepo)
+	if err != nil {
+		return testAddons, err
+	}
+
+	addons, err := cat.ListAddons()
+	if err != nil {
+		return testAddons, err
+	}
+
+	for _, addon := range addons {
+		for _, name := range names {
+			overrides(addon[0])
+			if addon[0].GetName() == name {
+				testAddons = append(testAddons, addon[0])
+			}
+		}
+	}
+
+	if len(testAddons) != len(names) {
+		return testAddons, fmt.Errorf("got %d addons, expected %d", len(testAddons), len(names))
+	}
+
+	return testAddons, nil
+}
+
+func findUnhandled() ([]v1beta1.AddonInterface, error) {
+	var unhandled []v1beta1.AddonInterface
+	repo, err := local.NewRepository("base", "../addons")
+	if err != nil {
+		return unhandled, err
+	}
+	addons, err := repo.ListAddons()
+	if err != nil {
+		return unhandled, err
+	}
+
+	for _, revisions := range addons {
+		addon := revisions[0]
+		found := false
+		for _, v := range addonTestingGroups {
+			for _, name := range v {
+				if name == addon.GetName() {
+					found = true
+				}
+			}
+		}
+		if !found {
+			unhandled = append(unhandled, addon)
+		}
+	}
+
+	return unhandled, nil
+}
+
+func kubectl(args ...string) error {
+	cmd := exec.Command("kubectl", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// -----------------------------------------------------------------------------
+// Private - CI Values Overrides
+// -----------------------------------------------------------------------------
+
+// TODO: a temporary place to put configuration overrides for addons
+// See: https://jira.mesosphere.com/browse/DCOS-62137
+func overrides(addon v1beta1.AddonInterface) {
+	if v, ok := addonOverrides[addon.GetName()]; ok {
+		addon.GetAddonSpec().ChartReference.Values = &v
+	}
+}
+
+var addonOverrides = map[string]string{
+	"metallb": `
+---
+configInline:
+  address-pools:
+  - name: default
+    protocol: layer2
+    addresses:
+    - "172.17.1.200-172.17.1.250"
+`,
+}
---
+++ test/go.mod
@@ -0,0 +1,21 @@
+module github.com/mesosphere/kubeaddons-kommander-addons/test
+
+go 1.13
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/mesosphere/kubeaddons v0.9.2
+	go.uber.org/atomic v1.5.1 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
+	go.uber.org/zap v1.13.0 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v3 v3.0.0-20191120175047-4206685974f2
+	k8s.io/apiextensions-apiserver v0.0.0-20191121021419-88daf26ec3b8 // indirect
+	k8s.io/utils v0.0.0-20191114200735-6ca3b61696b6 // indirect
+	sigs.k8s.io/kind v0.7.0
+)
+
+replace k8s.io/client-go => k8s.io/client-go v0.0.0-20191016111102-bec269661e48

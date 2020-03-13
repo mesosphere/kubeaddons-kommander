ed8550e27a6f0c81fda36e787784c643c4ba5f13
Shane Utt <shaneutt@linux.com>
2020-03-13 08:59:55 -0400

    Cleanup tests
    The tests here were getting messy, this separates things into
    logical groupings of functionality, including a file specifically
    for testing kommander, and one for testing thanos.
    
    Also marked some functionality that belongs upstream.

---
+++ test/setup_test.go
@@ -0,0 +1,60 @@
+package test
+
+import (
+	"github.com/mesosphere/kubeaddons/pkg/api/v1beta1"
+	"github.com/mesosphere/kubeaddons/pkg/catalog"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/git"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/local"
+	"github.com/mesosphere/kubeaddons/pkg/test"
+)
+
+// -----------------------------------------------------------------------------
+// Private - Test Setup
+// -----------------------------------------------------------------------------
+
+func init() {
+	var err error
+	localRepo, err = local.NewRepository("local", "../addons/")
+	if err != nil {
+		panic(err)
+	}
+
+	kbaRepo, err := git.NewRemoteRepository(kbaURL, kbaRef, kbaRemote)
+	if err != nil {
+		panic(err)
+	}
+
+	cat, err = catalog.NewCatalog(localRepo, kbaRepo)
+	if err != nil {
+		panic(err)
+	}
+
+	groups, err = test.AddonsForGroupsFile("groups.yaml", cat)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// -----------------------------------------------------------------------------
+// Private - Test Setup - CI Values Overrides
+// -----------------------------------------------------------------------------
+
+// TODO: a temporary place to put configuration overrides for addons
+// See: https://jira.mesosphere.com/browse/DCOS-62137
+func overrides(addon v1beta1.AddonInterface) {
+	if v, ok := addonOverrides[addon.GetName()]; ok {
+		addon.GetAddonSpec().ChartReference.Values = &v
+	}
+}
+
+var addonOverrides = map[string]string{
+	"metallb": `
+---
+configInline:
+  address-pools:
+  - name: default
+    protocol: layer2
+    addresses:
+    - "172.17.1.200-172.17.1.250"
+`,
+}
